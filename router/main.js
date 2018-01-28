93e000cad6a8e4205459c8e101e0dfee7f437d13
Jeon_heechun <heechun@getawair.com>
2018-01-28 13:56:38 +0900

    fix params

--- router/main.js
+++ router/main.js
@@ -3,12 +3,12 @@ module.exports = function(app)
      app.get('/',function(req,res){
         res.render('index.html')
      });
-     app.get('/exercise:id', function(req,res){
+     app.get('/exercise:id', function(req,res) {
         let souce = JSON.parse(fs.writeFileSync(`./exercise/${id}.json`));
         res.render('template', {
             title: souce.title,
             template: souce.template,
-            
-        })
+            test: souce.test
+        });
      });
 }
